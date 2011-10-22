66cb907d016920cf536d40c19daddb6c5059acfb
Conrad Irwin <conrad.irwin@gmail.com>
2011-10-22 01:48:22 -0700

    Rewrite to be source_location instead of __file__ and __line__

--- ext/ruby18_source_location.c
+++ ext/ruby18_source_location.c
@@ -69,113 +69,74 @@ struct BLOCK {
     struct BLOCK *prev;
 };
 
-/*
- * call-seq:
- *    meth.__file__  => String  
- *
- * returns the filename containing this method's definition
- *
- * raises ArgumentError if method has no associated ruby source code
- *
- * <i>Only available when MBARI_API extentions are enabled at build time</i>
- */
- 
 static VALUE
-method_source_file_name(VALUE method)
+source_pair(const char *filename, int lineno)
 {
-    struct METHOD *data;
-    NODE *node;
+    VALUE array = rb_ary_new();
+    rb_array_push(array, rb_str_new2(filename));
+    rb_array_push(array, INT2FIX(lineno));
 
-    Data_Get_Struct(method, struct METHOD, data);   
-    if (node = data->body) {
-      const char *filename = node->nd_file;
-      if (filename)
-        return rb_str_new2(filename);
-    }
-    rb_raise(rb_eArgError, "native Method");
+    return array;
+}
+
+static VALUE
+node_source_location(NODE *node)
+{
+    const char *filename = node->nd_file;
+    int lineno = nd_line(node);
+
+    if (filename && lineno)
+        return source_pair(filename, lineno);
+
+    return Qnil;
 }
 
 /*
  * call-seq:
- *    meth.__line__  => Fixnum  
+ *    meth.source_location  => [String, Fixnum]
  *
- * returns the starting line number of this method
+ * returns a pair of the Filename and line number on which the method is defined
  *
- * raises ArgumentError if method has no associated ruby source code
- *
- * <i>Only available when MBARI_API extentions are enabled at build time</i>
+ * returns nil if method has no associated ruby source code
  */
- 
 static VALUE
-method_source_line(VALUE method)
+method_source_location(VALUE method)
 {
     struct METHOD *data;
     NODE *node;
 
     Data_Get_Struct(method, struct METHOD, data);
     if (node = data->body) {
-      int lineno = nd_line(node);
-      if (lineno)
-        return INT2FIX(nd_line(node));
+        return node_source_location(node);
     }
-    rb_raise(rb_eArgError, "native Method");
+    return Qnil;
 }
-
 
 /*
  * call-seq:
- *    prc.__file__  => String  
+ *    meth.source_location  => [String, Fixnum]
  *
- * returns the filename where this proc is defined
+ * returns a pair of the Filename and line number on which the method is defined
  *
- * raises ArgumentError if proc has no associated ruby source
- *
- * <i>Only available when MBARI_API extentions are enabled at build time</i>
+ * returns nil if method has no associated ruby source code
  */
- 
 static VALUE
-proc_source_file_name(VALUE block)
-{
-    struct BLOCK *data;
-    const char *filename;
-    NODE *node;
-
-    Data_Get_Struct(block, struct BLOCK, data);
-    if ((node = data->frame.node) || (node = data->body))
-      return rb_str_new2(node->nd_file);
-    rb_raise(rb_eArgError, "native Proc");
-}
-
-
-/*
- * call-seq:
- *    prc.__line__  => Fixnum  
- *
- * returns the starting line number of this proc
- *
- * raises ArgumentError if proc has no associated ruby source code
- *
- * <i>Only available when MBARI_API extentions are enabled at build time</i>
- */
- 
-static VALUE
-proc_source_line(VALUE block)
+proc_source_location(VALUE block)
 {
     struct BLOCK *data;
     NODE *node;
 
     Data_Get_Struct(block, struct BLOCK, data);
-    if ((node = data->frame.node) || (node = data->body))
-      return INT2FIX( nd_line(node) );
-    rb_raise(rb_eArgError, "native Proc");
+    if ((node = data->frame.node) || (node = data->body)) {
+        return node_source_location(node);
+    }
+    return Qnil;
 }
+
 void
 Init_ruby18_source_location()
 {
-    rb_define_method(rb_cUnboundMethod, "__file__", method_source_file_name, 0);
-    rb_define_method(rb_cUnboundMethod, "__line__", method_source_line, 0);
-    rb_define_method(rb_cProc, "__file__", proc_source_file_name, 0);
-    rb_define_method(rb_cProc, "__line__", proc_source_line, 0);
-    rb_define_method(rb_cMethod, "__file__", method_source_file_name, 0);
-    rb_define_method(rb_cMethod, "__line__", method_source_line, 0);
+    rb_define_method(rb_cUnboundMethod, "source_location", method_source_location, 0);
+    rb_define_method(rb_cMethod, "source_location", method_source_location, 0);
+    rb_define_method(rb_cProc, "source_location", proc_source_location, 0);
 }
